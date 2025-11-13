afa96b415dac6646ff5f9ffad8a11524e38515b7
JhonSnow <jhon.vallecilla@alumnos.ucn.cl>
2025-11-13 20:47:51 -0300

    fix: enable durable option in RabbitMQ to persist queues

--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -70,6 +70,7 @@ builder.Services.AddMassTransit(x =>
         cfg.Publish<StockAlertMessage>(e =>
         {
             e.ExchangeType = "topic";
+            e.Durable = true;
         });
 
         cfg.Send<StockAlertMessage>(s =>
@@ -86,6 +87,7 @@ builder.Services.AddMassTransit(x =>
         cfg.Publish<OrderFailedStockMessage>(e =>
         {
             e.ExchangeType = "topic";
+            e.Durable = true;
         });
 
         cfg.Send<OrderFailedStockMessage>(s =>
@@ -100,12 +102,16 @@ builder.Services.AddMassTransit(x =>
         {
             // Configurar el consumer
             e.ConfigureConsumer<OrderCreatedConsumer>(context);
+
+            e.Durable = true;
+            e.AutoDelete = false;
             
             // Bind a la cola con el routing key order.created
             e.Bind("order_events", x =>
             {
                 x.RoutingKey = "order.created";
                 x.ExchangeType = "topic";
+                x.Durable = true;
             });
         });
 
